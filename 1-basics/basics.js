1bb3c0ae866c3ac9121f6e113c3af082cf14a554
unknown <danmwenda96@gmail.com>
2019-11-10 13:18:38 +0300

    11 commit

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -187,7 +187,7 @@ describe("basics", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-           
+           const secondArray = firstArray; 
             ////////////////////////////////////////////////////////////////////
 
             expect(secondArray).to.eql([1, 2, 3]);
@@ -209,6 +209,7 @@ describe("basics", function() {
             ////////////////////////////////////////////////////////////////////
             // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
             // ✏️ ADD CODE HERE ✏️
+            const secondArray = [1, 2, 3];
 
             ////////////////////////////////////////////////////////////////////
 
@@ -230,6 +231,7 @@ describe("basics", function() {
             ////////////////////////////////////////////////////////////////////
             // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
             // ✏️ ADD CODE HERE ✏️
+            const longArray = Array(100);
 
             ////////////////////////////////////////////////////////////////////
 
