99c57b8426270355594643d126f28f28122fe06b
unknown <danmwenda96@gmail.com>
2019-11-28 21:18:40 +0300

    function6 commit

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -98,6 +98,10 @@ describe("functions", function() {
             // and 'answerThirdResult' with the correct results.
             // If you call getContext() here, you're a cheater! :)
             // ✏️ ADD CODE HERE ✏️
+            let answerFirstResult = undefined;
+            let answerSecondResult = context;
+            let answerThirdResult = undefined;
+
 
             ////////////////////////////////////////////////////////////////////
 
