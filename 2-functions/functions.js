0c0fcae108b2238c1752cd86a5235cc71d9074b2
unknown <danmwenda96@gmail.com>
2019-11-28 21:29:00 +0300

    function14 commit

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -324,6 +324,9 @@ describe("functions", function() {
             ////////////////////////////////////////////////////////////////////
             // Can you implement a function 'myFunction' that returns a function?
             // ✏️ ADD CODE HERE ✏️
+            function myFunction(){
+                return function () {};
+            }
 
             ////////////////////////////////////////////////////////////////////
 
