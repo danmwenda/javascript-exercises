367b147b3d7e0da305c158364aec051ac3c4651b
unknown <danmwenda96@gmail.com>
2019-11-28 22:10:25 +0300

    strings1 commit

--- 4-standard-lib/strings.js
+++ 4-standard-lib/strings.js
@@ -14,6 +14,9 @@ describe("strings", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+            function findIndexOf(string) {
+                return text.indexOf(string);
+            }
 
             ////////////////////////////////////////////////////////////////////
 
@@ -28,6 +31,9 @@ describe("strings", function() {
 
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE. USE THE STANDARD LIB! ✏️
+            function findLastIndexOf(string) {
+                return text.lastIndexOf(string);
+            }
 
             ////////////////////////////////////////////////////////////////////
 
