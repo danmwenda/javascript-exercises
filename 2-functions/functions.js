782d963eb507514aa1472737c7c7b357c79bb7d4
unknown <danmwenda96@gmail.com>
2019-11-28 21:32:36 +0300

    function16 commit

--- 2-functions/functions.js
+++ 2-functions/functions.js
@@ -370,6 +370,7 @@ describe("functions", function() {
                 // Uh-oh, the function 'breakTheLoop' calls itself recursively (see below).
                 // There are two possible solutions that break the loop. Can you find both?
                 // ✏️ ADD CODE HERE ✏️
+                return;
 
                 ////////////////////////////////////////////////////////////////////
 
